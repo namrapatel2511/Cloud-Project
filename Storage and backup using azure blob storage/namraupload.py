a5d2215c6320c21b56fdc745a832e97ad2fdf7ee
Namra Patel <117341135+namrapatel2511@users.noreply.github.com>
2024-06-24 17:36:25 +0530

    namraupload.py

---
+++ Storage and backup using azure blob storage/namraupload.py
@@ -0,0 +1,19 @@
+from azure.storage.blob import BlobServiceClient
+import os
+
+
+#change cridentials detils with your's details 
+storage_account_key = "sXZHmfAtgr3V3JAEgkTYO9TwHXWS8ZNt8Q*****************************************************"
+storage_account_name = "namra2511"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=namra2511;AccountKey=sXZHmf********************************************************************************t"
+container_name = "namra"
+
+def uploadToBlobStorage(file_path, file_name):
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client =blob_service_client.get_blob_client(container=container_name,blob=file_name)
+
+    with open(file_path,"rb") as data:
+        blob_client.upload_blob(data)
+        print("Upload "+file_name+" file")
+
+uploadToBlobStorage('C:\\Users\\namra\\OneDrive\\Documents\\cloud\\task.png','cloud')
